449773ff901a351dbae43b51e57928ac9f70830e
MeghanaGudaram <meghaleo92@gmail.com>
2017-02-25 15:52:25 -0500

    Update Assignment2_WriteMemory.cpp

--- Assignment2_WriteMemory.cpp
+++ Assignment2_WriteMemory.cpp
@@ -22,29 +22,27 @@ __m256 avx_memoryWrite(float* array, int size)
 int main()
 {
 #pragma omp parallel
-{
+	{
+		static int size=1000000; // size of 4000000 bytes and not 40MB
+		float *array;
+		int status=posix_memalign((void**) &array, 32 , size*sizeof(float));
+		for(int j=0;j<size;j++)
+			array[j]=1;
+		float bw=0;
+		__m256 val= _mm256_set1_ps(0);
+	
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
+		for(int i=0;i<100;i++)
+    		{
+			val=avx_memoryWrite(array,size); // function call to make sure code is not optimized
+    		}
+    		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    		duration<double> time_span = duration_cast<duration<double>> (t2 - t1);
+    		//bw = (size*4*100)/(1000000000 * time_span.count());
+    		printf(" Time taken for 100 operations of %d bytes : %lf BW = %lf GB/s\n",size*4,time_span.count(), (size*4)/(10000000 * time_span.count()));
+    		printf("value %f\n",val[0]);
 
-    static int size=1000000; // size of 4000000 bytes and not 40MB
-    float *array=(float*)malloc(sizeof(float) * size);
-    for(int j=0;j<size;j++)
-    array[j]=1;
-    float bw=0;
-    __m256 val= _mm256_set1_ps(0);
-
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
-
-    for(int i=0;i<100;i++)
-    {
-		val=avx_memoryWrite(array,size); // function call to make sure code is not optimized
-    }
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    duration<double> time_span = duration_cast<duration<double>> (t2 - t1);
-
-    bw = (size*4*100)/(1000000000 * time_span.count());
-    printf(" Time taken for 100 operations of %d bytes : %lf BW = %lf GB/s\n",size*4,time_span.count(), bw);
-    printf("value %f\n",val[0]);
-
-}
+	}
 
     return 0;
 }
